95697a3eb16086c85c51f491acd8b21a8e93196d
Daniel260601 <47327755+Daniel260601@users.noreply.github.com>
2022-01-15 13:43:45 +0100

    First Menu and methods. Contains logic error and needs to be solved

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    static java.util.Scanner scanner = new java.util.Scanner((System.in));
+
+    static void firstHashtag(){
+        System.out.print("Enter First Custom Hashtag: ");
+        String firstCustomHashtagInput = scanner.nextLine();
+        String firstCustomHashtag = firstCustomHashtagInput.toLowerCase();
+        String firstFinalHashtag = "#" + firstCustomHashtag;
+        System.out.println(firstFinalHashtag);
+        Boolean firstHashtagUsed = true;
+    }
+
+    static void secondHashtag(){
+        System.out.print("Enter Second Custom Hashtag: ");
+        String secondCustomHashtagInput = scanner.nextLine();
+        String secondCustomHashtag = secondCustomHashtagInput.toLowerCase();
+        String secondFinalHashtag = "#" + secondCustomHashtag;
+        System.out.println(secondFinalHashtag);
+        Boolean secondHashtagUsed = true;
+    }
+
+    static void thirdHashtag(){
+        System.out.print("Enter Third Custom Hashtag: ");
+        String thirdCustomHashtagInput = scanner.nextLine();
+        String thirdCustomHashtag = thirdCustomHashtagInput.toLowerCase();
+        String thirdFinalHashtag = "#" + thirdCustomHashtag;
+        System.out.println(thirdFinalHashtag);
+        Boolean thirdHashtagUsed = true;
+    }
+
+    //Current method does not work, because booleans and other variables can't be used for a status.
+
+    public static void main(String[] args) {
+
+
+        boolean endProgram = false;
+        while (!endProgram) {
+            System.out.println();
+            System.out.println();
+            System.out.println("Main Menu");
+            System.out.println("---------");
+            System.out.println();
+            System.out.println("    F  -> Enter First Custom Hashtag");
+            System.out.println("    S  -> Enter Second Custom Hashtag");
+            System.out.println("    T  -> Enter Third Custom Hashtag");
+            System.out.println("    O  -> Print Hashtags");
+            System.out.println("\n  E  -> Exit program");
+
+
+            System.out.print("\nChoice: ");
+            String input2 = scanner.nextLine().toUpperCase();
+            switch (input2) {
+                case "F" -> firstHashtag();
+                case "S" -> secondHashtag();
+                case "T" -> thirdHashtag();
+//                case "O" ->
+                case "E" -> endProgram = true;
+                default -> System.out.println("Invalid Choice!");
+            }
+        }
+    }
+}
